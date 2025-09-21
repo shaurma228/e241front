3269e074cec21f2b8251de3e06c8cf66f753e8c3
shaurmator <sirnikdziha@gmail.com>
2025-09-21 18:33:06 +0500

    testing /api/home/test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
 'use client'
 
-import React from "react"
+import React, { useEffect, useState } from "react"
 import withAuth from "@/hoc/WithAuth"
+import axios from "axios"
+
+const apiUrl = process.env.NEXT_PUBLIC_API_URL
 
 function Home() {
+    const [testText, setTestText] = useState('')
+
+    const fetchTest = async () => {
+        try {
+            const response = await axios.get(`${apiUrl}/api/home/test`)
+            setTestText(response.data.message)
+        } catch (error) {
+            console.error('Error fetching test data', error)
+        }
+    }
+
+    useEffect(() => {
+        fetchTest()
+    }, [])
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-
+            {testText}
         </div>
     )
 }
